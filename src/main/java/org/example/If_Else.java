63eca5bf499d2ce8ccb6456a13c3e4052be241e4
StaceyO21 <staceyobrien21@gmail.com>
2025-07-20 19:23:27 -0500

    short hand if/else

--- src/main/java/org/example/If_Else.java
+++ src/main/java/org/example/If_Else.java
@@ -11,13 +11,19 @@ public class If_Else {
 
 //
 
-    int time = 22;
+//    int time = 22;
+//
+//    if (time < 10) {
+//        System.out.println("Good morning");
+//    } else if (time < 18) {
+//        System.out.println("Good day");
+//    } else {
+//        System.out.println("Good evening");
+//    }
 
-    if (time < 10) {
-        System.out.println("Good morning");
-    } else if (time < 18) {
-        System.out.println("Good day");
-    } else {
-        System.out.println("Good evening");
-    }
+//    short hand if....else
+
+    int time = 20;
+    String result = (time < 18) ? "Good day" : "Good evening";
+    System.out.println(result);
 }
