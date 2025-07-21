7f076f106b317109bf899c80d6581f6f9f7aa61e
StaceyO21 <staceyobrien21@gmail.com>
2025-07-20 19:21:15 -0500

    if/else if/else

--- src/main/java/org/example/If_Else.java
+++ src/main/java/org/example/If_Else.java
@@ -9,11 +9,15 @@ public class If_Else {
 //        System.out.println("x is greater than y");
 //    }
 
-    int time = 20;
+//
 
-    if (time < 18) {
-        System.out.println("Good Day");
+    int time = 22;
+
+    if (time < 10) {
+        System.out.println("Good morning");
+    } else if (time < 18) {
+        System.out.println("Good day");
     } else {
-        System.out.println("Good Evening");
+        System.out.println("Good evening");
     }
 }
