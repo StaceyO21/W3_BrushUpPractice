26d1ee99b95b99c62c712a788fbd864bd51214b2
StaceyO21 <staceyobrien21@gmail.com>
2025-07-20 19:18:28 -0500

    if/else statements

--- src/main/java/org/example/If_Else.java
+++ src/main/java/org/example/If_Else.java
@@ -2,10 +2,18 @@ package org.example;
 
 public class If_Else {
 
-    int x = 20;
-    int y = 18;
+//    int x = 20;
+//    int y = 18;
+//
+//    if(x > y) {
+//        System.out.println("x is greater than y");
+//    }
 
-    if(x > y) {
-        System.out.println("x is greater than y");
+    int time = 20;
+
+    if (time < 18) {
+        System.out.println("Good Day");
+    } else {
+        System.out.println("Good Evening");
     }
 }
